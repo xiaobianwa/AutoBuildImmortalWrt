a4bda5d0a6b31c6810d322ede95cfbf6f22ebfd4
xiaobianwa <58133433+xiaobianwa@users.noreply.github.com>
2025-05-06 12:56:18 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -101,7 +101,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by NiaorenGuiyu"
+NEW_DESCRIPTION="Compiled by NiaoRenGuiyu"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
